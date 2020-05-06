370b4d9266a701482a4641a6781dc9813aa84009
gautamamber <ambergautam1@gmail.com>
2020-05-06 11:10:40 +0530

    feat():start app

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+# third party imports
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# local imports
+
+from config import app_config
+
+# db variable initialize
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    """
+    Initialize application with env
+    :param config_name:
+    :return:
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+    return app
---
+++ instance/config.py
@@ -0,0 +1,2 @@
+SECRET_KEY = "this is random secret key"
+SQLALCHEMY_DATABASE_URL = 'mysql://dt_admin:dt2016@localhost/employee_db'
