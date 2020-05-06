7d67695c345e72d78f892890ea8e1a553a5ad625
gautamamber <ambergautam1@gmail.com>
2020-05-06 16:40:50 +0530

    feat():add models test cases

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import unittest
+from flask import abort, url_for
 from flask_testing import TestCase
 from app import create_app, db
-from app.models import Employee
+from app.models import Employee, Role, Department
 
 
 class TestBase(TestCase):
@@ -17,7 +18,7 @@ class TestBase(TestCase):
         config_name = 'testing'
         app = create_app(config_name)
         app.config.update(
-            SQLALCHEMY_DATABASE_URI='mysql://dt_admin:dt2016@localhost/dreamteam_test'
+            SQLALCHEMY_DATABASE_URI='mysql://dt_admin:dt2020@localhost/dreamteam_test'
         )
         return app
 
@@ -26,7 +27,7 @@ class TestBase(TestCase):
         Call this before every test
         :return:
         """
-        db.creat_all()
+        db.create_all()
         # create test admin user
         admin = Employee(username='admin', password='admin202', is_admin=True)
         employee = Employee(username="test_user", password="test2016")
@@ -41,6 +42,39 @@ class TestBase(TestCase):
         """
         db.session.remove()
         db.drop_all()
+
+
+class TestModels(TestBase):
+    """
+    Test models
+    """
+
+    def test_employee_model(self):
+        """
+        Test employee model, count number of records
+        :return:
+        """
+        self.assertEqual(Employee.query.count(), 2)
+
+    def test_department_model(self):
+        """
+        test department model, count number records
+        :return:
+        """
+        department = Department(name="IT", description="It department")
+        db.session.add(department)
+        db.session.commit()
+        self.assertEqual(Department.query.count(), 1)
+
+    def test_role_model(self):
+        """
+        test department model, count number records
+        :return:
+        """
+        role = Role(name="CEO", description="CEO role")
+        db.session.add(role)
+        db.session.commit()
+        self.assertEqual(Role.query.count(), 1)
 
 
 if __name__ == "__main__":
