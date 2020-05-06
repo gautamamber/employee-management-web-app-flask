2fa5d048bfa9e48329a52504d257b3d56ac37ea0
gautamamber <ambergautam1@gmail.com>
2020-05-06 15:41:06 +0530

    feat():admin assign role and departments to employee

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from app.models import Department, Role
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
 
 class DepartmentForm(FlaskForm):
@@ -18,4 +20,15 @@ class RoleForm(FlaskForm):
     """
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EmployeeAssignForm(FlaskForm):
+    """
+    Form for admin to assign departments and roles to employee
+    """
+    department = QuerySelectField(query_factory=lambda: Department.query.all(),
+                                  get_label="name")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(),
+                            get_label="name")
     submit = SubmitField('Submit')
